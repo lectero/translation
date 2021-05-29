378b09f4012df12c4426a6a151ac5ef3217d181e
Foks <37844753+FoksVHox@users.noreply.github.com>
2021-05-29 15:00:20 +0200

    Create auth.php

---
+++ da/auth.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'failed'   => 'Disse legitimationsoplysninger svarer ikke til vores optegnelser.',
+    'throttle' => 'For mange login forsøg. Prøv igen om :seconds sekunder.',
+    'login'    => 'Log på',
+    'register' => 'Tilmeld',
+    'logout'   => 'Log ud'
+];
